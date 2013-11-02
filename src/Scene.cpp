e1f1e77a1d205c9b698b9d3a30461d96612e1566
Hung Leong <xblitzerx@gmail.com>
2013-11-02 16:08:32 -0700

    init distance check method in scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -58,3 +58,21 @@ void updateScene() {
 
 
 }
+
+
+void distanceCheck(){
+	updateBallPositions();
+	bool done = false;
+
+	while(!done){
+		for (int i = 0; i < balls.size(); i++){
+			for (int j = 0; j < balls.size(); j++)
+			{
+				Sphere s1* = balls.at(i);
+				Sphere s2* = balls.at(j);
+
+				if()
+			}
+		}
+	}
+}
