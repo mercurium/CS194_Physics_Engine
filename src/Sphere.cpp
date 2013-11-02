9521a9de85729936e823f46821550d565d76882a
Hung Leong <xblitzerx@gmail.com>
2013-11-02 15:28:02 -0700

    Sphere class coded
    Added sphere class, constructor, get set methods

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,9 +1,60 @@
 #include "Sphere.h"
 
-Sphere::Sphere(){
+Sphere::Sphere(double x, double y, double z){
+	position = glm::vec3(x, y, z);
+	velocity = glm::vec3(0.0, 0.0, 0.0);
+	acceleration = glm::vec3(0.0, 0.0, 0.0);
 
+	double radius = 1;
+	double mass = 1;
+}
+
+Sphere::Sphere(double x, double y, double z, double r, double m){
+	position = glm::vec3(x, y, z);
+	velocity = glm::vec3(0.0, 0.0, 0.0);
+	acceleration = glm::vec3(0.0, 0.0, 0.0);
+
+	double radius = r;
+	double mass = m;
 }
 
 Sphere::~Sphere (){
+	delete position;
+	delete radius;
+	delete mass;
+	delete velocity;
+	delete acceleration;
 
+}
+
+void Sphere::setPos(glm::vec3 pos){
+	position = pos;
+}
+
+void Sphere::setVelocity(glm::vec3 vel){
+	velocity = vel;
+}
+
+void Sphere::setAcceleration(glm::vec3 acc){
+	acceleration = acc;
+}
+
+glm::vec3 Sphere::getPos(){
+	return position;
+}
+
+glm::vec3 Sphere::getVelocity(){
+	return velocity;
+}
+
+glm::vec3 Sphere::getAcceleration(){
+	return acceleration;
+}
+
+double Sphere::getRadius(){
+	return radius;
+}
+
+double Sphere::getMass(){
+	return mass;
 }
--- src/Sphere.h
+++ src/Sphere.h
@@ -1,14 +1,23 @@
 #pragma once
 #include <glm/glm.hpp>
 
-class Sphere {
-    public:
-        Sphere();
-        virtual ~Sphere ();
-        void draw(); //draw sphere in OpenGL/whatever we are going to use.
+class Sphere{
+	glm::vec3 position;
+	glm::vec3 velocity;
+	glm::vec3 acceleration;
 
-    private:
-        /* data */
-        int x,y,z; //Position
-        int r; //radius
+	double radius;
+	double mass;
+	
+	public:
+		Sphere(double x, double y, double z);
+		Sphere(double x, double y, double z, double r, double m);
+		virtual ~Sphere(); 
+		void setPos(double x, double y, double z);
+		void setVelocity(double v_x, double v_y, double v_z);
+		void getPos();
+		void getVelocity();
+		void getAcceleration();
+		void getRadius();
+		void getMass();
 };
