605f4e5b086674307bf43cf5f7e3d9baf108f957
Hung Leong <xblitzerx@gmail.com>
2013-11-02 15:38:12 -0700

    Sphere.h debug

--- src/Sphere.h
+++ src/Sphere.h
@@ -13,11 +13,13 @@ class Sphere{
 		Sphere(double x, double y, double z);
 		Sphere(double x, double y, double z, double r, double m);
 		virtual ~Sphere(); 
+		
 		void setPos(double x, double y, double z);
 		void setVelocity(double v_x, double v_y, double v_z);
-		void getPos();
-		void getVelocity();
-		void getAcceleration();
-		void getRadius();
-		void getMass();
+		
+		glm::vec3 getPos();
+		glm::vec3 getVelocity();
+		glm::vec3 getAcceleration();
+		double getRadius();
+		double getMass();
 };
