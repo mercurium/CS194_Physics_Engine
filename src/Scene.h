206200575e875a78985c331409d29c5dc63115bf
Jerry Chen <jerrychen434@gmail.com>
2013-11-03 21:11:59 -0800

    incomplete work pushing to branch to monitor progress

--- src/Scene.h
+++ src/Scene.h
@@ -2,16 +2,18 @@
 #include <vector>
 #include "Intersection.h"
 #include "Sphere.h"
-
+#include "DistConstr.h"
 
 class Scene{
+    std::vector<Sphere *> balls;
+    std::vector<DistConstr *> distContraints;
     public:
         Scene(void);
         ~Scene(void);
         std::vector<Intersection> getIntersections(Sphere &obj);
+        void UpdateBallPositions();
+        void UpdateScene();
 
     private:
-        std::vector<Sphere *> balls;
-        std::vector<DistConstr *> distContraints;
 
 };
