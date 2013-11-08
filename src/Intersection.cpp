04789407c3f4a8c5280d66a45ecf33e5a4ccd1b7
Jerry Chen <jerrychen434@gmail.com>
2013-11-07 16:17:38 -0800

    this is a temporary branch, code is not complete, reformatting code for collisions

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -1,5 +1,11 @@
 #include "Intersection.h"
 
+Intersection::Intersection(Sphere *s1, Sphere *s2, double dist){
+	this.s1 = s1;
+	this.s2 = s2;
+	this.dist = dist;
+}
+
 Intersection::Intersection(){
     //Constructor
 }
