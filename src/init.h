da1e129b8ae7c7b90d0906dcc78d52a9b46b29a2
Hung <Leong>
2013-11-09 22:32:48 -0800

    added more functionality, new sphere constructor

--- src/init.h
+++ src/init.h
@@ -5,6 +5,22 @@
 #include <map>
 #include <vector>
 
+#include "Sphere.h"
+#include "DistConstr.h"
+
+
+#include <glm/glm.hpp>
+
 class Init{
-	std::vector<char> v;
+	std::vector<Sphere*> *balls;
+	std::vector<Lights*> *lights
+	std::vector<DistConstr*> *constraints;
+	std::map<string, int> *dict;
+
+	public:
+		void initialize(std::vector<Sphere*> *balls, std::vector<Lights*> *lights, std::map<string, int> dict);
+
+	private:
+		bool readvals(stringstream &s, const int numvals, double * values);
+		void readfile(const char * filename);
 }
