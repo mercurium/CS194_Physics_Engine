da004fc65894219f55f813bc76850501b954af61
Hung <Leong>
2013-12-02 02:06:48 -0800

    more simd conv to sphere

--- src/Sphere.h
+++ src/Sphere.h
@@ -3,29 +3,29 @@
 #include <glm/gtx/simd_vec4.hpp>
 
 class Sphere{
-	detail::fvec4SIMD position;
-	detail::fvec4SIMD oldPos;
-	detail::fvec4SIMD velocity;
+	glm::detail::fvec4SIMD position;
+	glm::detail::fvec4SIMD oldPos;
+	glm::detail::fvec4SIMD velocity;
     glm::vec4 color;
 
 	double radius;
 	double mass;
 	
 	public:
-		Sphere(detail::fvec4SIMD pos, detail::fvec4SIMD vel);
+		Sphere(glm::detail::fvec4SIMD pos, glm::detail::fvec4SIMD vel);
 		Sphere(double x, double y, double z);
 		Sphere(double x, double y, double z, bool twoD);
 		Sphere(double x, double y, double z, double r);
 		virtual ~Sphere(); 
 		
-		void setPos(detail::fvec4SIMD pos);
-		void setOldPos(detail::fvec4SIMD oldPosition);
-		void setVelocity(detail::fvec4SIMD vel);
+		void setPos(glm::detail::fvec4SIMD pos);
+		void setOldPos(glm::detail::fvec4SIMD oldPosition);
+		void setVelocity(glm::detail::fvec4SIMD vel);
 		void setColor(glm::vec4 col);
 		
-		detail::fvec4SIMD getPos();
-		detail::fvec4SIMD getOldPos();
-		detail::fvec4SIMD getVelocity();
+		glm::detail::fvec4SIMD getPos();
+		glm::detail::fvec4SIMD getOldPos();
+		glm::detail::fvec4SIMD getVelocity();
 		glm::vec4 getColor();
 		
 		double getRadius();
